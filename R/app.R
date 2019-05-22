6be7e5f862a825e068d12f4872cdf57ae354fa04
Kaitlan McKinney <50498865+KaitlanM@users.noreply.github.com>
2019-05-22 14:53:06 +0200

    Working on intermediate task

--- R/app.R
+++ R/app.R
@@ -1,8 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
+### Package info:
 # You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
@@ -13,17 +9,39 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-# Define the player's score and input the data
-
+### Packages used:
 #install.packages("shiny")
+library(shiny)
+#install.packages("plotrix")
+library(plotrix)
+
+### Plot random number of circles for intermediate activity
+
+plot(1:10, type = "n", bty = "o", xlab = "", ylab = "", main = "") # The empty plot
+
+numCirc <- sample(20:50, 1) # The number of circles
+
+for (i in 1:numCirc){ # Plot them
+  draw.circle(runif(1, min = 1, max = 10), runif(1, min = 1, max = 10), radius = 0.4,
+              col = rgb(red = runif(1), green = runif(1), blue = runif(1))
+              )
+}
+
+
+
+
 library(shiny)
 ui <- navbarPage(title = "Memory Measurer",
                  tabPanel("Memorizing Phase",
-                         "Memorize the following words: bat, rat, fat, tap"
+                         "Memorize the following words:"
                          ),
                  tabPanel("Intermediate Task",
-                           sliderInput(inputId = "circleGuess", label = "Count the circles and indicate on the slider how many there are.",
-                          min = 0, max = 100, value = 0)
+                           sliderInput(inputId = "circleGuess",
+                                       label = "Count the circles and indicate on the slider how many there are.",
+                                       min = 0, max = 100, value = 0),
+                          plotOutput("circles")
+
+
                           ),
                  tabPanel("Reciting",
                           textInput(inputId = "wordsRemembered",
@@ -32,7 +50,18 @@ ui <- navbarPage(title = "Memory Measurer",
 
 )
 
-server <- function(input, output){}
+server <- function(input, output){
+  output$circles <- renderPlot(1:10, type = "n") # The empty plot
+
+                                numCirc <- sample(20:50, 1) # The number of circles
+
+                                for (i in 1:numCirc){ # Plot them
+                                  draw.circle(runif(1, min = 1, max = 10), runif(1, min = 1, max = 10), radius = 0.4,
+                                              col = rgb(red = runif(1), green = runif(1), blue = runif(1))
+                                  )
+                                }
+
+}
 
 shinyApp(ui = ui, server = server)
 
